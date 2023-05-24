4d89966fd53e492ecae2d54957279beeb07bb2a2
lexand41 <lexand41@yandex.ru>
2023-05-24 14:49:18 +0700

    lesson02

---
+++ crm/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
+
+console.log(modalTitle);
+console.log(modalForm);
+console.log(modalCheckbox);
+console.log(modalInputDiscount);
