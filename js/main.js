bf509711728c95c09901cc5666f273f14f0f976f
lexand41 <lexand41@yandex.ru>
2023-05-28 11:44:10 +0700

    change "let" to "const"

--- js/main.js
+++ js/main.js
@@ -63,66 +63,62 @@ const goods = [
 ];
 
 const createRow = (arrow) => {
-  let row = document.createElement('tr');
+  const row = document.createElement('tr');
 
-  let row_num = document.createElement('td');
+  const row_num = document.createElement('td');
   row_num.classList.add('table__cell');
   row_num.innerHTML = number;
-  row.appendChild(row_num);
+  
 
-  let span_id = document.createElement('span');
+  const span_id = document.createElement('span');
   span_id.classList.add('table__cell-id');
   span_id.textContent = `id: ${arrow.id}`;
 
-  let row_id = document.createElement('td');
+  const row_id = document.createElement('td');
   row_id.innerHTML = arrow.title;
   row_id.classList.add('table__cell', 'table__cell_left', 'table__cell_name')
   row_id.setAttribute('data-id', arrow.id);
   row_id.insertAdjacentElement('afterbegin', span_id);
-  row.appendChild(row_id);
 
-  let row_category = document.createElement('td');
+  const row_category = document.createElement('td');
   row_category.classList.add('table__cell', 'table__cell-left');
   row_category.innerHTML = arrow.category;
-  row.appendChild(row_category);
 
-  let row_units = document.createElement('td');
+  const row_units = document.createElement('td');
   row_units.classList.add('table__cell');
   row_units.innerHTML = arrow.units;
-  row.appendChild(row_units);
 
-  let row_count = document.createElement('td');
+  const row_count = document.createElement('td');
   row_count.classList.add('table__cell');
   row_count.innerHTML = arrow.count;
-  row.appendChild(row_count);
 
-  let row_price = document.createElement('td');
+  const row_price = document.createElement('td');
   row_price.classList.add('table__cell');
   row_price.innerHTML = `$${arrow.price}`;
-  row.appendChild(row_price);
 
-  let row_total = document.createElement('td');
+  const row_total = document.createElement('td');
   row_total.classList.add('table__cell');
   row_total.innerHTML = `$${arrow.price * arrow.count}`;
-  row.appendChild(row_total);
 
-  let button_pic = document.createElement('button');
+  const button_pic = document.createElement('button');
   button_pic.classList.add('table__btn', 'table__btn_pic');
 
-  let button_edit = document.createElement('button');
+  const button_edit = document.createElement('button');
   button_edit.classList.add('table__btn', 'table__btn_edit');
 
-  let button_del = document.createElement('button');
+  const button_del = document.createElement('button');
   button_del.classList.add('table__btn', 'table__btn_del');
 
-  let row_button = document.createElement('td');
+  const row_button = document.createElement('td');
   row_button.classList.add('table__cell', 'table__cell_btn-wrapper');
-  row_button.appendChild(button_pic);
-  row_button.appendChild(button_edit);
-  row_button.appendChild(button_del);
-  row.appendChild(row_button);
+  row_button.append(button_pic);
+  row_button.append(button_edit);
+  row_button.append(button_del);
 
-  table.appendChild(row);
+  row.append(row_num, row_id, row_category,
+    row_units, row_count, row_price, row_total, row_button);
+
+  table.append(row);
 
   console.log(row);
 };
